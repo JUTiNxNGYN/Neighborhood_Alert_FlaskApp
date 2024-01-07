64f62adc82e5264e2c99aae5d34134f516bf7848
JUTiNxNGYN <JUTiNxNGYN@gmail.com>
2024-01-07 06:47:43 -0600

    third commit

--- Justin_Flask_App.py
+++ Justin_Flask_App.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from uuid import uuid4
 
 app = Flask(__name__)
 
@@ -14,16 +15,43 @@ users = {
 }
 
 posts = {
-    '1' = {
-        'body': 'FLASK WEEK LETS GO!!!',
+    '1' :  {
+        'body': 'FLASK WEEK PROJECT',
         'user_id': '1'
     },
-    '2' = {
+    '2' : {
         'body': 'Whiteboard was killer',
         'user_id': '2'
     },
-    '3' = {
+    '3' : {
         'body': 'SERVERS!!!',
-        'user_id': '3'
+        'user_id': '1'
     }
 }
+
+#user routes
+
+# @app.route('/user', methods=['GET'])
+# def user():
+#     return { 'users': list(users.values())}
+
+@app.get('/user')
+def user():
+    return { 'users': list(users.values())}, 200
+
+@app.post('/user')
+def create_user():
+    json_body = request.get_json()
+    users[uuid4()] = json_body
+    return { 'message': f'{json_body["username"]} created'}, 201 #Good, created
+
+
+# @app.post('/user', methods=["GET", "POST"])
+# def user2():
+#     return { 'users': list(users.values())}
+
+#post routes  
+
+
+
+ 
