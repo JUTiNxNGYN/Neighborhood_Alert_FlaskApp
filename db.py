1c32b817bcebb6493e86792f9d8d3b22440f298e
JUTiNxNGYN <JUTiNxNGYN@gmail.com>
2024-01-15 15:25:28 -0600

    added jwt

--- db.py
+++ db.py
@@ -2,29 +2,36 @@
 users = {
   '1': {
     'username': 'jnguyen',
-    'email' : 'jnguyen@gmail.com'
+    'email' : 'jnguyen@gmail.com',
+    'password' : '123'
   },
   '2' : {
     'username': "nnguyen",
-    'email': 'nnguyen@yahoo.com'
+    'email': 'nnguyen@yahoo.com',
+    'password' : '345'
+
   },
   '3' : {
     'username': "mnguyen",
-    'email' : 'mnguyen@hotmail.com'
+    'email' : 'mnguyen@hotmail.com',
+    'password' : '456'
   }
 }
 
 posts = {
   '1' : {
-    'body' : "Dog lost!!",
+    'title' : "Dog lost!!",
+    'body' : "My dog has been lost",
     'user_id': '1'
   },
   '2': {
-    'body': "Cold Freeze Warning",
+    'title': "Cold Freeze Warning",
+    'body' : "Sub-freezing temperature as low as 0 degrees",
     'user_id': '3'
   },
   '3':{
-    'body': "Theif, Stole my packages",
+    'title': "Thief, Stole my packages",
+    'body' : "Thief stole my package from porch, please help!!",
     'user_id' : '2'
   }
 }
