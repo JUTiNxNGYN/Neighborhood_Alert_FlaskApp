4dc7cd1dd37bb355271680759d887eaa863ad653
JUTiNxNGYN <JUTiNxNGYN@gmail.com>
2024-01-15 14:48:32 -0600

    jwt security

--- resources/user/routes.py
+++ resources/user/routes.py
@@ -1,55 +1,81 @@
 from flask import request
-from uuid import uuid4
+
 from flask.views import MethodView
+from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask_smorest import abort
-
-from models import PostModel
-from schemas import PostSchema, PostSchemaNested
-
 from . import bp
-# post routes
 
-@bp.route('/<post_id>')
-class Post(MethodView):
+from schemas import UserSchema, UserSchemaNested
+from models.user_model import UserModel
+# user routes
 
-  @bp.response(200, PostSchemaNested)
-  def get(self, post_id):
-    post = PostModel.query.get(post_id)
-    if post:
-      print(post.author)
-      return post 
-    abort(400, message='Invalid Post')
+@bp.route('/user/<user_id>')
+class User(MethodView):
 
-  @bp.arguments(PostSchema)
-  def put(self, post_data ,post_id):
-    post = PostModel.query.get(post_id)
-    if post:
-      post.body = post_data['body']
-      post.commit()   
-      return {'message': 'post updated'}, 201
-    return {'message': "Invalid Post Id"}, 400
-    
-  def delete(self, post_id):
-    post = PostModel.query.get(post_id)
-    if post:
-      post.delete()
-      return {"message": "Post Deleted"}, 202
-    return {'message':"Invalid Post"}, 400
+  @bp.response(200, UserSchemaNested)
+  def get(self,user_id):
+    user = UserModel.query.get(user_id)
+    if user:
+      print(user.posts.all())
+      return user
+    else:
+      abort(400, message='User not found')
 
-@bp.route('/')
-class PostList(MethodView):
+  @jwt_required()  
+  @bp.arguments(UserSchema)
+  def put(self, user_data, user_id):
+    user = UserModel.query.get(get_jwt_identity())
+    if user and user.id == user_id:
+      user.from_dict(user_data)
+      user.commit()
+      return { 'message': f'{user.username} updated'}, 202
+    abort(400, message = "Invalid User")
 
-  @bp.response(200, PostSchema(many = True))
+  @jwt_required()
+  def delete(self, user_id):
+    user = UserModel.query.get(get_jwt_identity())
+    if user == user_id:
+      user.delete()
+      return { 'message': f'User: {user.username} Deleted' }, 202
+    return {'message': "Invalid username"}, 400
+
+@bp.route('/user')
+class UserList(MethodView):
+
+  @bp.response(200, UserSchema(many = True))
   def get(self):
-    return PostModel.query.all()
+   return UserModel.query.all()
   
-  @bp.arguments(PostSchema)
-  def post(self, post_data):
-    try:
-      post = PostModel()
-      post.user_id = post_data['user_id']
-      post.body = post_data['body']
-      post.commit()
-      return { 'message': "Post Created" }, 201
+  @bp.arguments(UserSchema)
+  def post(self, user_data):
+    try: 
+      user = UserModel()
+      user.from_dict(user_data)
+      user.commit()
+      return { 'message' : f'{user_data["username"]} created' }, 201
     except:
-      return { 'message': "Invalid User"}, 401
+      abort(400, message='Username and Email Already taken')
+      
+@bp.route('/user/follow/<followed_id>')
+class FollowUser(MethodView):
+
+  @jwt_required()
+  def post(self, followed_id):
+    followed = UserModel.query.get(followed_id)
+    follower =UserModel.query.get(get_jwt_identity())
+    if follower and followed:
+      follower.follow(followed)
+      followed.commit()
+      return {'message':'user followed'}
+    else:
+      return {'message':'invalid user'}, 400
+  @jwt_required()  
+  def put(self, followed_id):
+    followed = UserModel.query.get(followed_id)
+    follower = UserModel.query.get(get_jwt_identity())
+    if follower and followed:
+      follower.unfollow(followed)
+      followed.commit()
+      return {'message':'user unfollowed'}
+    else:
+      return {'message':'invalid user'}, 400
