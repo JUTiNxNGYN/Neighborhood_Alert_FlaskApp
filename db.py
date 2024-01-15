af99ef9665403d770bc5cb1e2ad21db9d30872c0
JUTiNxNGYN <JUTiNxNGYN@gmail.com>
2024-01-15 13:54:31 -0600

    all crud ops

---
+++ db.py
@@ -0,0 +1,30 @@
+
+users = {
+  '1': {
+    'username': 'jnguyen',
+    'email' : 'jnguyen@gmail.com'
+  },
+  '2' : {
+    'username': "nnguyen",
+    'email': 'nnguyen@yahoo.com'
+  },
+  '3' : {
+    'username': "mnguyen",
+    'email' : 'mnguyen@hotmail.com'
+  }
+}
+
+posts = {
+  '1' : {
+    'body' : "Dog lost!!",
+    'user_id': '1'
+  },
+  '2': {
+    'body': "Cold Freeze Warning",
+    'user_id': '3'
+  },
+  '3':{
+    'body': "Theif, Stole my packages",
+    'user_id' : '2'
+  }
+}
